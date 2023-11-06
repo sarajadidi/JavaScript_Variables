f5ce20b24eb61c5a8774249291ab31a8b7477a2c
sarajadidi <sararivkajadidi@gmail.com>
2023-11-06 08:08:21 -0800

    finished exercise

--- app.js
+++ app.js
@@ -4,7 +4,26 @@ console.log(
 );
 
 // Exercise 1
-
+let lastName
+var age
 // Exercise 2
-
+const firstName = "Sara"
+lastName = "Jadidi"
+age = 20
 // Exercise 3
+let language = "JavaScript"
+let createdYear = 1995
+let isCaseSensitive = true
+// Exercise 4
+let price = 19.99
+let isOnSale = false
+let salePercentage = 15
+let stock = 0
+let inStock = false
+let selectedSize = "M"
+//Exercise 5
+let title = "Name of the Wind"
+let author = "Patrick Rothfuss"
+let pageCount = 772
+let bookmark = 456
+let hasRead = true
